86c0424d0cb7b0e5c01341ab054400fcba2d10af
Casey Occhialini <1508707+littlespex@users.noreply.github.com>
2023-01-20 18:48:55 -0800

    no message

--- conversion/index.mjs
+++ conversion/index.mjs
@@ -8,7 +8,7 @@ await setup();
 // convert source code to ES6
 const { toEs6 } = await import('./toEs6.mjs');
 await toEs6('lib/**/*.js');
-await cmd('npx eslint ./lib/**/*.js --fix --ignore-pattern=lib/goog/**');
+await cmd(`npx eslint ./lib/**/*.js --fix --ignore-pattern=lib/goog/** --rule 'require-jsdoc: [off]'`);
 
 // teardown
 // await teardown();
